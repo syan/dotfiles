cdab45977e1495c3588f91a143d5158594f36445
syan <cn.syan@gmail.com>
2012-06-06 22:09:38 +0900

    setup neobundle

--- scripts/vimsetup.sh
+++ scripts/vimsetup.sh
@@ -4,3 +4,12 @@ echo "create vim temporary dir [~/.vimtmp]"
 mkdir -p ~/.vimtmp/backup-files/ 
 mkdir -p ~/.vimtmp/vimswp/
 
+mkdir -p ~/.vim/bundle
+if [ ! -d ~/.vim/bundle/neobundle ] ; then
+	echo "clone neobundle"
+	git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle
+	mkdir -p ~/.vim/bundle/neobundle/plugins/
+
+	vim -c ":NeoBundleInstall"
+fi
+
